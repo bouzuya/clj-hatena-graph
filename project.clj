e065262682ccf711bcd2ba8af2ac952d10dc9b2a
bouzuya <bouzuya@gmail.com>
2013-04-28 03:03:00 +0900

    version 0.1.0 -> 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.bouzuya/hatena.graph "0.1.0"
+(defproject org.clojars.bouzuya/hatena.graph "0.2.0"
   :description "Hatena::Graph API for Clojure."
   :url "http://github.com/bouzuya/clj-hatena-graph"
   :license {:name "Eclipse Public License"
